dfaa3379ef7a4f8d15f0e646a7a1adf6b5dba494
abhilekh singh <abhilekhsingh041992@gmail.com>
2016-05-23 10:39:34 +0600

    Final commit :sunglasses:

--- Solution.java
+++ Solution.java
@@ -1,1 +1,56 @@
-5967c3dc-20a0-11e6-ba78-a0999b0023fb
+package test;
+
+// you can also use imports, for example:
+import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+
+
+class Solution {
+    static public int solution(int[] A) {
+        // write your code in Java SE 8
+
+        int n = A.length;
+        int[] cnt = new int[n];
+        int k = 0;
+        for (int elem : A) {
+            cnt[elem]++;
+            if(cnt[elem] == 1) k++;
+        }
+        Arrays.fill(cnt, 0);
+
+        int curr = 0;
+        int result = n;
+        for(int i=0, j=0;j<n;j++) {
+            cnt[A[j]]++;
+            if(cnt[A[j]] == 1) curr++;
+
+            if(curr == k) {
+                result = Math.min(result, (j-i+1));
+                while(i <= j) {
+                    if(cnt[A[i]] == 1) break;
+                    cnt[A[i]]--;
+                    i++;
+                    result = Math.min(result, (j-i+1));
+                }
+            }
+        }
+
+        return  result;
+    }
+
+    int a=20;
+    static int b=10;
+    void m1()
+    {
+        Solution t= new Solution();
+        System.out.println(t.b);
+    }
+    public static void main(String args[])
+    {
+        Solution t1= new Solution();
+        t1.m1();
+    }
+}
